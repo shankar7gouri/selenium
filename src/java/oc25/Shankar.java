09a125c0a8987d326360dcc3bb7873a18a276130
SHANKAR <shankar7gouri@gmail.com>
2019-11-20 08:21:13 +0530

    oct25

---
+++ src/java/oc25/Shankar.java
@@ -0,0 +1,20 @@
+package java.oc25;
+
+public class Shankar {
+	
+	public void method1() {
+		System.out.println("method1");
+	}
+	public void method2() {
+		System.out.println("method2");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shankar x = new Shankar();
+		x.method1();
+		x.method2();
+
+	}
+
+}
---
+++ src/java/oc26/shankar.java
@@ -0,0 +1,11 @@
+package java.oc26;
+
+public class shankar {
+  public static void main (String[] args){
+		int n, c;
+		n=2;
+		System.out.println("Multiplication table" + n);
+		for (c=1;c<=10;c++);
+		System.out.println(n+ "*" +c+"="+(n*c));
+  }
+}
